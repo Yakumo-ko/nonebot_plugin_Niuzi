430efe2316f39775527347c259991bab5d5bae54
Yakumo_ko <2501390802@qq.com>
2023-01-16 02:15:15 +0800

    add a command

---
+++ commands/ChangeSex.py
@@ -0,0 +1,15 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.matcher import Matcher
+from nonebot.adapters.mirai2 import GroupMessage 
+from nonebot.params import Arg, CommandArg, ArgPlainText
+
+from ..service.ChangeSexService import ChangeSexService
+
+change_woman = on_command("变女性", rule=to_me())
+
+@change_woman.handle()
+async def handle_first_receive \
+        (matcher: Matcher, event: GroupMessage):
+    await matcher.finish(ChangeSexService().change2Woman(event.get_user_id()))
+
---
+++ service/ChangeSexService.py
@@ -0,0 +1,9 @@
+
+class ChangeSexService:
+
+    def change2Woman(self, qq: str) -> str:
+        assert False
+
+    def change2Man(self, qq: str) -> str:
+        assert False
+
